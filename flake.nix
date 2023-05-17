2cb6d4afd5ff8a93644031807b65ff24191f5d5a
c4710n <c4710n@users.noreply.github.com>
2023-05-17 12:10:03 +0800

    deps: lock deps

---
+++ flake.nix
@@ -0,0 +1,22 @@
+{
+  description = "A flake for building development environment of Go project.";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/master";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in
+      with pkgs; {
+        devShells.default = mkShell {
+          buildInputs = [
+            go
+          ];
+        };
+      }
+    );
+}
